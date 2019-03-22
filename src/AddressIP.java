c6f212d8100b92299272ff66104b65e0abd4b384
Tomasz Smolen <tomaszsmolen97@gmail.com>
2019-03-22 19:06:24 +0100

    Add important methods

--- src/AddressIP.java
+++ src/AddressIP.java
@@ -1,31 +1,36 @@
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+
 public class AddressIP {
     private InetAddress localHost;
+    private InetAddress hostAddressToPing = null;
+    private String address;
+    private int subnetMask;
+    private int network, broadcast;
+
+    public InetAddress getHostAddressToPing() {
+        return hostAddressToPing;
+    }
 
     public String getAddress() {
         return address;
     }
 
-    public int getSubnetMask() {
-        return subnetMask;
-    }
-
-    private String address;
-    private int subnetMask;
     public void addressValidation(String commandLine) throws UnknownHostException, SocketException {
-        if(verifyAddress(commandLine) == false){
-            System.out.println("Address is not valid! Script will get PC IP address.");
+        if(!verifyAddress(commandLine)){
+            System.out.println("Address is not valid! Script will get PC IP address." + System.getProperty("line.separator"));
             localHost = InetAddress.getLocalHost();
             NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost);
             address = localHost.getHostAddress();
+            hostAddressToPing = InetAddress.getByName(address);
             subnetMask = networkInterface.getInterfaceAddresses().get(0).getNetworkPrefixLength();
         }
         else{
-            System.out.println("Address is valid!");
+            System.out.println("Address is valid!" + System.getProperty("line.separator"));
             String[] parts = commandLine.split("/");
             address = parts[0];
             subnetMask = Integer.parseInt(parts[1]);
@@ -69,9 +74,9 @@ public class AddressIP {
         }
         return maskDecimal;
     }
-    public String addressIpToBinary(){
+    public String addressIpToBinary(String addresTo){
         String addressIpBinary = new String();
-        String[] addresBinaryParts = address.split("\\.");
+        String[] addresBinaryParts = addresTo.split("\\.");
         for(int i = 0;i<addresBinaryParts.length;i++){
             int parsedDecimal = Integer.toBinaryString(Integer.parseInt(addresBinaryParts[i])).length();
             while (parsedDecimal != 8){
@@ -85,5 +90,85 @@ public class AddressIP {
         }
         return addressIpBinary;
     }
-
+    public int addressToInt(String addressTo){
+        int addressInt = 0;
+        String[] addressParts = addressTo.split("\\.");
+        for (int i = 3; i >= 0; i--) {
+            long ip = Long.parseLong(addressParts[3 - i]);
+            addressInt |= ip << (i * 8);
+        }
+        return addressInt;
+    }
+    public String intToAddress(int addressTo){
+        StringBuilder addressIP = new StringBuilder(15);
+        for (int i = 0; i < 4; i++) {
+            addressIP.insert(0,(addressTo & 0xff));
+            if (i < 3) {
+                addressIP.insert(0,'.');
+            }
+            addressTo = addressTo >> 8;
+        }
+        return addressIP.toString();
+    }
+    public String calculateNetworkAddress(String addressTo){
+        network = (addressToInt(addressTo) & addressToInt(subnetMaskDecimal()));
+        return intToAddress(network);
+    }
+    public String calculateBroadcastAddress(){
+        broadcast = (network | ~(addressToInt(subnetMaskDecimal())));
+        return intToAddress(broadcast);
+    }
+    public String firstHostAddress(){
+        return intToAddress(network | 1);
+    }
+    public String lastHostAddress(){
+        return intToAddress(broadcast ^ 1);
+    }
+    public int maxHostAmount(){
+        int maxHostNumber;
+        maxHostNumber = (broadcast ^ 1) - (network);
+        return maxHostNumber;
+    }
+    public void isPrivate (String addressTo){
+        int addressToInt = addressToInt(addressTo);
+        String newLine = System.getProperty("line.separator");
+        if(addressToInt >= addressToInt("10.0.0.0") &&  addressToInt <= addressToInt("10.255.255.255")){
+            System.out.println("Podany adres jest adresem prywatnym." + newLine);
+        }
+        else if(addressToInt >= addressToInt("172.16.0.0") &&  addressToInt <= addressToInt("172.31.255.255")){
+            System.out.println("Podany adres jest adresem prywatnym." + newLine);
+        }
+        else if(addressToInt >= addressToInt("192.168.0.0") &&  addressToInt <= addressToInt("192.168.255.255")){
+            System.out.println("Podany adres jest adresem prywatnym." + newLine);
+        }
+        else{
+            System.out.println("Podany adres jest adresem publicznym." + newLine);
+        }
+    }
+    public void whatClassOfAddress(String addressTo){
+        int addressToInt = addressToInt(addressTo);
+        String newLine = System.getProperty("line.separator");
+        if(addressToInt >= addressToInt("0.0.0.0") &&  addressToInt <= addressToInt("127.255.255.255")){
+            System.out.println("Podany adres należy do klasy A." + newLine);
+        }
+        else if(addressToInt >= addressToInt("128.0.0.0") &&  addressToInt <= addressToInt("191.255.255.255")){
+            System.out.println("Podany adres należy do klasy B." + newLine);
+        }
+        else if(addressToInt >= addressToInt("192.0.0.0") &&  addressToInt <= addressToInt("223.255.255.255")){
+            System.out.println("Podany adres należy do klasy C." + newLine);
+        }
+        else if(addressToInt >= addressToInt("224.0.0.0") &&  addressToInt <= addressToInt("239.255.255.255")){
+            System.out.println("Podany adres należy do klasy D." + newLine);
+        }
+        else{
+            System.out.println("Podany adres należy do klasy E." + newLine);
+        }
+    }
+    public void sendPingRequest() throws IOException {
+        System.out.println("Sending ping request to: " + hostAddressToPing);
+        if(hostAddressToPing.isReachable(5000))
+            System.out.println("Host: " + hostAddressToPing +  " is reachable!");
+        else
+            System.out.println("Can't reach to this host!");
+    }
 }
--- src/subnetCalculator.java
+++ src/subnetCalculator.java
@@ -1,16 +1,35 @@
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class subnetCalculator {
-    public static void main(String[] args) throws UnknownHostException, SocketException {
+    public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter IP address (a.b.c.d/mask): ");
         String commandLine = in.nextLine();
         AddressIP addressIP = new AddressIP();
         addressIP.addressValidation(commandLine);
-        //System.out.println("Maska dziesiętnie: " + addressIP.subnetMaskDecimal() + ", maska binarnie: " + addressIP.subnetMaskBinary());
-        System.out.println("Adres IP binarnie: " + addressIP.addressIpToBinary());
+        String myIpAddress = addressIP.getAddress();
+        String networkAddress = addressIP.calculateNetworkAddress(myIpAddress);
+        String broadcastAddress = addressIP.calculateBroadcastAddress();
+        String firstHostAddress = addressIP.firstHostAddress();
+        String lastHostAddress = addressIP.lastHostAddress();
+        String newLine = System.getProperty("line.separator");
+        System.out.println("Adres sieci: ");
+        System.out.println("Dziesiętnie: " + networkAddress + ", binarnie: " + addressIP.addressIpToBinary(networkAddress) + newLine);
+        System.out.println("Maska sieci: ");
+        System.out.println("Dziesiętnie: " + addressIP.subnetMaskDecimal() + ", binarnie: " + addressIP.subnetMaskBinary() + newLine);
+        System.out.println("Adres broadcast: ");
+        System.out.println("Dziesiętnie: " + broadcastAddress + ", binarnie: " + addressIP.addressIpToBinary(broadcastAddress) + newLine);
+        System.out.println("Pierwszy adres hosta: ");
+        System.out.println("Dziesiętnie: " + firstHostAddress + ", binarnie: " + addressIP.addressIpToBinary(firstHostAddress) + newLine);
+        System.out.println("Ostatni adres hosta: ");
+        System.out.println("Dziesiętnie: " + lastHostAddress + ", binarnie: " + addressIP.addressIpToBinary(lastHostAddress)+ newLine);
+        System.out.println("Maksymalna liczba hostów: " + addressIP.maxHostAmount() + newLine);
+        addressIP.isPrivate(myIpAddress);
+        addressIP.whatClassOfAddress(myIpAddress);
+        if(addressIP.getHostAddressToPing() != null){
+            addressIP.sendPingRequest();
+        }
     }
 }
 
